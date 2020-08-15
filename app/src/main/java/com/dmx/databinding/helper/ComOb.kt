6f8f0fc3bfeddf7e57358314a64af149c30e96e1
xxxue <1659809758@qq.com>
2020-08-15 15:39:39 +0800

    ..

--- app/src/main/java/com/dmx/databinding/helper/ComOb.kt
+++ app/src/main/java/com/dmx/databinding/helper/ComOb.kt
@@ -2,13 +2,16 @@ package com.dmx.databinding.helper
 
 import androidx.databinding.ObservableField
 
-open class ComOb<T>(default:T) : ObservableField<T>() {
+open class ComOb<T>(default: T) : ObservableField<T>(default) {
 
 
-    var value: T = default
+    var value: T = this.get()!!
         set(value) {
             field = value
             this.set(value)     //注意，这个this.set才是ObservableField原有的方法，即我们之前直接调用的方法
+        }
+        get() {
+            return this.get()!!
         }
 
     class String(default: kotlin.String = "") : ComOb<kotlin.String>(default)
